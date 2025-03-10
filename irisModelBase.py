7fbd2ac71903bcd79df490e3e5d50978e665c0ad
108-0 <2023143029@g.shingu.ac.kr>
2025-03-10 16:03:39 +0900

    첫번째 커밋 fastapi를 사용하여 완료

--- irisModelBase.py
+++ irisModelBase.py
@@ -3,9 +3,11 @@
 # 용희가 만
 from operator import irshift
 
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 iris_df = pd.read_csv('iris.csv')
 
@@ -14,14 +16,28 @@ x= iris_df.drop('species', axis=1)
 
 kn = KNeighborsClassifier()
 model_kn = kn.fit(x,y)
+rfc = RandomForestClassifier()
+model_rfc = rfc.fit(x,y)
+
+joblib.dump(model_rfc, 'model_rfc.pkl')
 
 #x_new = np.array([[3,3,3,3]])
-x_new = np.array([[5.0,3.4,1.4,0.2]])
-# ['setosa'] 3 ([[1. 0. 0,]])
-# kn ['versicolor'][[[0.0.8 0.2]]
-# kn['versicolor']([[1,4.2,1.4,7]])
+# kn ['versicolor']([[0. 0.8 0.2]])
+x_new = np.array([[5.0 ,3.4, 1.4, 0.2]])
+# rfc ['setosa']  ([[1. 0. 0,]])
+# kn ['setosa']  ([[1. 0. 0,]])
+
+#x_new = np.array([[1, 4.2, 1.4, 7]])
+# kn['versicolor']([[0.2 0.6 0.2]])
+# rfc ['setosa'] ([[0.52 0.29 0.19]])
+
+model_rfc = joblib.load('model_rfc.pkl')
+
 prediction = model_kn.predict(x_new)
+perdiction = model_rfc.predict(x_new)
 print(prediction)
+
 probability = model_kn.predict_proba(x_new)
+probability = model_rfc.predict_proba(x_new)
 print(probability)
 
